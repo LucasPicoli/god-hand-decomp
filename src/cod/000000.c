3a07cdebd863cd3dec3848ee06b0c654fcd3be08
Lucas Picoli <LucasPicoli@users.noreply.github.com>
2026-06-26 16:31:44 -0700

    match: +1 function (func_0034D318, drop-freorder struct copy)

--- src/cod/000000.c
+++ src/cod/000000.c
@@ -3221,3 +3221,4 @@ INCLUDE_ASM("nonmatching", SearchData);
 
 
 
+
---
+++ src/cod/0034d318.c
@@ -0,0 +1,5 @@
+/* cygnus-2.96 matched TU. */
+
+struct S { int x[549]; };
+__attribute__((section(".text.func_0034D318")))
+void func_0034D318(struct S *a, struct S *b) { *a = *b; }
