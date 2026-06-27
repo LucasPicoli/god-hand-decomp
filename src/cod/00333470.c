e8cc48871047c65a99461556673ffbc30da6c2dc
Lucas Picoli <LucasPicoli@users.noreply.github.com>
2026-06-27 13:25:54 -0700

    match: +2 functions (cygnus twins)

---
+++ src/cod/00333470.c
@@ -0,0 +1,15 @@
+/* cygnus-2.96 matched TU. */
+
+__attribute__((section(".text.func_00333470")))
+int func_00333470(int a0, int a1) {
+    signed char b = a1;
+    int r = func_00333438((signed char)a0);
+    return (r ^ func_00333438(b)) ? -1 : 0;
+}
+
+__attribute__((section(".text.func_00334E98")))
+int func_00334E98(int a0, int a1) {
+    signed char b = a1;
+    int r = func_00334E80((signed char)a0);
+    return (r ^ func_00334E80(b)) ? -1 : 0;
+}
