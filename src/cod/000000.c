8be3cc5fa18bdb2c8285bf64c8354416c3aa36ad
Lucas Picoli <LucasPicoli@users.noreply.github.com>
2026-07-02 20:34:42 -0700

    match: +1 functions (func_00343A98)

--- src/cod/000000.c
+++ src/cod/000000.c
@@ -3275,3 +3275,4 @@ INCLUDE_ASM("nonmatching", SearchData);
 
 
 
+
---
+++ src/cod/00343a98.c
@@ -0,0 +1,16 @@
+/* cygnus-2.96 matched TU. */
+
+__attribute__((section(".text.func_00343A98")))
+int func_00343A98(unsigned int a0) {
+    switch (a0) {
+    case 2:
+    case 3:
+        return 0;
+    case 0:
+    case 1:
+    case 4:
+    case 5:
+    default:
+        return 1;
+    }
+}
