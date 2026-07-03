d4d6a438d46eebe6cb0e22f809afd881f41f9217
Lucas Picoli <LucasPicoli@users.noreply.github.com>
2026-07-03 07:18:42 -0700

    match: +1 functions (func_0033E648)

--- src/cod/000000.c
+++ src/cod/000000.c
@@ -3296,3 +3296,4 @@ INCLUDE_ASM("nonmatching", SearchData);
 
 
 
+
---
+++ src/cod/0033e648.c
@@ -0,0 +1,12 @@
+/* cygnus-2.96 matched TU. */
+
+extern volatile int D_003EADB0;
+extern int D_003EADA8;
+
+__attribute__((section(".text.func_0033E648")))
+void func_0033E648(void) {
+    D_003EADB0--;
+    if (D_003EADB0 == 0) {
+        D_003EADA8 = 0;
+    }
+}
