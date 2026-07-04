8d9b81602b39cac8e577c3c11bcb3e6fe2cea1ab
Lucas Picoli <LucasPicoli@users.noreply.github.com>
2026-07-04 04:27:33 -0700

    match: +1 functions (func_0027DB50: external-rodata jumptable class, sn, via extern_jtbl)

---
+++ src/cod/0027db50.c
@@ -0,0 +1,45 @@
+/* sn-2.95.3-136 matched TU. */
+
+__attribute__((section(".text.func_0027DB50")))
+void func_0027DB50(char *obj, unsigned int mode)
+{
+    switch (mode) {
+    case 0:
+    default:
+        obj[0x2F4] = 0;
+        obj[0x2F5] = 6;
+        obj[0x2F6] = 0;
+        obj[0x2F7] = 0;
+        break;
+    case 1:
+        obj[0x2F4] = 0;
+        obj[0x2F5] = 6;
+        obj[0x2F6] = 2;
+        obj[0x2F7] = 0;
+        break;
+    case 2:
+        obj[0x2F4] = 0;
+        obj[0x2F5] = 6;
+        obj[0x2F6] = 4;
+        obj[0x2F7] = 0;
+        break;
+    case 3:
+        obj[0x2F4] = 0;
+        obj[0x2F5] = 6;
+        obj[0x2F6] = 0;
+        obj[0x2F7] = 1;
+        break;
+    case 4:
+        obj[0x2F4] = 0;
+        obj[0x2F5] = 6;
+        obj[0x2F6] = 2;
+        obj[0x2F7] = 1;
+        break;
+    case 5:
+        obj[0x2F4] = 0;
+        obj[0x2F5] = 6;
+        obj[0x2F6] = 4;
+        obj[0x2F7] = 1;
+        break;
+    }
+}
