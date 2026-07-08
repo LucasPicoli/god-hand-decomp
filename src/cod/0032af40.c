4217fada4f8fa2b87cae93bd8805e7cd697fe68e
Lucas Picoli <LucasPicoli@users.noreply.github.com>
2026-07-08 12:13:46 -0700

    match: +3 functions (func_0032AF40..func_0032DE88)

---
+++ src/cod/0032af40.c
@@ -0,0 +1,30 @@
+/* cygnus-2.96 matched TU. */
+
+extern void NoOp_33E6A8(void);
+extern void func_0032AF80(void *a0, int a1);
+extern void NoOp_33E6B0(void);
+extern void func_0032B658(void *a0, int a1);
+extern void Obj33F8_NoOp_FCA0(void);
+extern void func_0032DEC8(int a0, int a1);
+extern void Obj33F8_NoOp_FCA8(void);
+
+__attribute__((section(".text.func_0032AF40")))
+void func_0032AF40(void *a0, int a1) {
+    NoOp_33E6A8();
+    func_0032AF80(a0, a1);
+    NoOp_33E6B0();
+}
+
+__attribute__((section(".text.func_0032B618")))
+void func_0032B618(void *a0, int a1) {
+    NoOp_33E6A8();
+    func_0032B658(a0, a1);
+    NoOp_33E6B0();
+}
+
+__attribute__((section(".text.func_0032DE88")))
+void func_0032DE88(int a0, int a1) {
+    Obj33F8_NoOp_FCA0();
+    func_0032DEC8(a0, a1);
+    Obj33F8_NoOp_FCA8();
+}
