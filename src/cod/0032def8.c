1022ba7e52518ac83fe00a3a06cd1c91a185445a
Lucas Picoli <LucasPicoli@users.noreply.github.com>
2026-07-08 12:14:57 -0700

    match: +1 functions (func_0032DEF8)

---
+++ src/cod/0032def8.c
@@ -0,0 +1,12 @@
+/* cygnus-2.96 matched TU. */
+
+extern void Obj33F8_NoOp_FCA0(void);
+extern void func_0032DF38(int a0, int a1);
+extern void Obj33F8_NoOp_FCA8(void);
+
+__attribute__((section(".text.func_0032DEF8")))
+void func_0032DEF8(int a0, int a1) {
+    Obj33F8_NoOp_FCA0();
+    func_0032DF38(a0, a1);
+    Obj33F8_NoOp_FCA8();
+}
