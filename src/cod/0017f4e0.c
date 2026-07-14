4f2de4771154eae7d3b4224dd67ae00219bf02cd
Lucas Picoli <LucasPicoli@users.noreply.github.com>
2026-07-14 11:03:47 -0700

    match: +1 functions (func_0017F4E0)

---
+++ src/cod/0017f4e0.c
@@ -0,0 +1,54 @@
+/* sn-2.95.3-136 matched TU. */
+
+extern char *InitFields_1B6E90(void *);
+extern int D_00422498;
+
+#include "godhand/vu0.h"
+
+
+
+
+__attribute__((section(".text.func_0017F4E0")))
+void *func_0017F4E0(char *a0) {
+    char *p;
+    char *q;
+    unsigned int i;
+
+    InitFields_1B6E90(a0);
+    p = a0 + 0x600;
+    *(int **)(a0 + 0x214) = &D_00422498;
+    VU0_SQC2_VF0(a0, 0x650);
+    VU0_SQC2_VF0(a0, 0x660);
+    VU0_SQC2_VF0(a0, 0x670);
+    VU0_SQC2_VF0(a0, 0x680);
+    VU0_SQC2_VF0(a0, 0x690);
+    VU0_SQC2_VF0(a0, 0x6A0);
+    VU0_SQC2_VF0(a0, 0x6B0);
+    VU0_SQC2_VF0(a0, 0x6C0);
+    VU0_SQC2_VF0(a0, 0x6D0);
+    VU0_SQC2_VF0(a0, 0x6E0);
+    VU0_SQC2_VF0(a0, 0x6F0);
+    VU0_SQC2_VF0(a0, 0x700);
+    VU0_SQC2_VF0(a0, 0x710);
+    VU0_SQC2_VF0(a0, 0x720);
+    p[0] = 0;
+    p[1] = 0;
+    p[2] = 0;
+    *(int *)(p + 0x4) = 0;
+    *(int *)(p + 0x8) = 0;
+    *(int *)(p + 0x30) = 0;
+    *(int *)(p + 0x34) = 0;
+    *(int *)(p + 0x38) = 0;
+    *(int *)(p + 0x3C) = 0;
+    *(int *)(p + 0x130) = 0;
+    *(int *)(p + 0x134) = 0;
+    *(int *)(p + 0x138) = 0;
+    *(int *)(p + 0x13C) = 0;
+    i = 0;
+    q = p + 0xC;
+    for (; i < 8; i++) {
+        *(int *)q = 0;
+        q += 4;
+    }
+    return a0;
+}
