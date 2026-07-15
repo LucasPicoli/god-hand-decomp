11f609268af8dbd9fe01f7aa43ba8efe34c7e843
Lucas Picoli <LucasPicoli@users.noreply.github.com>
2026-07-14 19:26:23 -0700

    match: +1 functions (cMessage_deleteAll)

---
+++ src/cod/002aec88.c
@@ -0,0 +1,13 @@
+/* sn-2.95.3-136 matched TU. */
+
+/* sn-2.95.3-136 matched TU. */
+
+
+
+__attribute__((section(".text.cMessage_deleteAll")))
+void cMessage_deleteAll(void *a0) {
+    int v = *(int *)((char *)a0 + 0x10);
+    while (v) {
+        v = func_002AEF10(a0, v);
+    }
+}
