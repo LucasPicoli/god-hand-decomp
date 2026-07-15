367070c5d59ccc9bba789584ef876c14a8ce89ff
Lucas Picoli <LucasPicoli@users.noreply.github.com>
2026-07-14 21:34:28 -0700

    match: +1 functions (func_002011B0)

---
+++ src/cod/002011b0.c
@@ -0,0 +1,14 @@
+/* sn-2.95.3-136 matched TU. */
+
+extern void func_00200B20(void *a0);
+
+/* sn-2.95.3-136 matched TU. */
+
+
+
+__attribute__((section(".text.func_002011B0")))
+void func_002011B0(void *a0) {
+    while (*(int *)a0) {
+        func_00200B20(a0);
+    }
+}
