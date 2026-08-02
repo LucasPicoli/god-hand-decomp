fb978e09490ce216503be11a8077ba585661617a
Lucas Picoli <LucasPicoli@users.noreply.github.com>
2026-08-02 12:09:43 -0700

    match: +8 functions (func_003B66C0..func_003B7C08)

---
+++ src/cod/003b66c0.c
@@ -0,0 +1,57 @@
+/* sn-2.95.3-136 matched TU. */
+
+#include "godhand/eesyscall.h"
+
+__attribute__((section(".text.func_003B66C0")))
+void func_003B66C0(void) {
+    EE_SYSCALL(0x5B);
+}
+
+#include "godhand/eesyscall.h"
+
+__attribute__((section(".text.func_003B6850")))
+void func_003B6850(void) {
+    EE_SYSCALL(0x74);
+}
+
+#include "godhand/eesyscall.h"
+
+__attribute__((section(".text.func_003B6860")))
+void func_003B6860(void) {
+    EE_SYSCALL(0x5A);
+}
+
+#include "godhand/eesyscall.h"
+
+__attribute__((section(".text.func_003B68A8")))
+void func_003B68A8(void) {
+    EE_SYSCALL(0x5B);
+}
+
+#include "godhand/eesyscall.h"
+
+__attribute__((section(".text.func_003B7AD8")))
+void func_003B7AD8(void) {
+    EE_SYSCALL(0x5A);
+}
+
+#include "godhand/eesyscall.h"
+
+__attribute__((section(".text.func_003B7B20")))
+void func_003B7B20(void) {
+    EE_SYSCALL(0x5B);
+}
+
+#include "godhand/eesyscall.h"
+
+__attribute__((section(".text.func_003B7B30")))
+void func_003B7B30(void) {
+    EE_SYSCALL(0x74);
+}
+
+#include "godhand/eesyscall.h"
+
+__attribute__((section(".text.func_003B7C08")))
+void func_003B7C08(void) {
+    EE_SYSCALL(0x56);
+}
