bd35d4adefa9f1668a55f2aa7efa1682a59c4486
Lucas Picoli <LucasPicoli@users.noreply.github.com>
2026-08-02 11:47:01 -0700

    match: +1 functions (func_0031F4A0)

---
+++ src/cod/0031f4a0.c
@@ -0,0 +1,19 @@
+/* ee-2.9-991111 matched TU. */
+
+extern void func_0031EEC8(void *a, void *b);
+extern int D_00460DA0;
+extern int D_004510B0;
+
+/* clone */
+
+
+
+
+__attribute__((section(".text.func_0031F4A0")))
+void *func_0031F4A0(void)
+{
+    if (D_00460DA0 == 0) {
+        func_0031EEC8(&D_00460DA0, &D_004510B0);
+    }
+    return &D_00460DA0;
+}
