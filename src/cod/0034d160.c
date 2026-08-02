b20ef7bd759d4da25b5fb3ef7c1c55dd78ddff1a
Lucas Picoli <LucasPicoli@users.noreply.github.com>
2026-08-02 11:32:09 -0700

    match: +1 functions (sfhds_IsPrvate2PketStart)

---
+++ src/cod/0034d160.c
@@ -0,0 +1,9 @@
+/* cygnus-2.96 matched TU. */
+
+/* clone */
+
+
+__attribute__((section(".text.sfhds_IsPrvate2PketStart")))
+int sfhds_IsPrvate2PketStart(void) {
+    return func_0034D188() == 447;
+}
