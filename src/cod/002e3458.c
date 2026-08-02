8a93a1b398ba8bb9c3e88a0b8ea07bb0983956c0
Lucas Picoli <LucasPicoli@users.noreply.github.com>
2026-08-02 11:28:13 -0700

    match: +10 functions (func_002E3458..func_002FFB10)

---
+++ src/cod/002e3458.c
@@ -0,0 +1,112 @@
+/* sn-2.95.3-136 matched TU. */
+
+extern char D_0044E238[];
+extern char D_0044E6D8[];
+extern char D_0044E770[];
+extern char D_0044E8C8[];
+extern char D_0044F078[];
+extern char D_0044F308[];
+extern char D_0044F4F8[];
+extern char D_0044F598[];
+extern char D_0044F6D8[];
+
+/* clone */
+
+
+
+__attribute__((section(".text.func_002E3458")))
+int func_002E3458(void *a0) {
+    *(char**)((char*)a0+0xF0) = D_0044E238;
+    return func_002FBE28(a0);
+}
+
+/* clone */
+
+
+
+__attribute__((section(".text.func_002E9B40")))
+int func_002E9B40(void *a0) {
+    *(char**)((char*)a0+0xF0) = D_0044E6D8;
+    return func_002FBE28(a0);
+}
+
+/* clone */
+
+
+
+__attribute__((section(".text.func_002EA8E0")))
+int func_002EA8E0(void *a0) {
+    *(char**)((char*)a0+0xF0) = D_0044E770;
+    return func_002FBE28(a0);
+}
+
+/* clone */
+
+
+
+__attribute__((section(".text.func_002EBF70")))
+int func_002EBF70(void *a0) {
+    *(char**)((char*)a0+0xF0) = D_0044E8C8;
+    return func_002EBC50(a0);
+}
+
+/* clone */
+
+
+
+__attribute__((section(".text.func_002F3E40")))
+int func_002F3E40(void *a0) {
+    *(char**)((char*)a0+0xF0) = D_0044F078;
+    return func_002FBE28(a0);
+}
+
+/* clone */
+
+
+
+__attribute__((section(".text.func_002F5328")))
+int func_002F5328(void *a0) {
+    *(char**)((char*)a0+0xF0) = D_0044F308;
+    return func_002FBE28(a0);
+}
+
+/* clone */
+
+
+
+__attribute__((section(".text.func_002F6230")))
+int func_002F6230(void *a0) {
+    *(char**)((char*)a0+0xF0) = D_0044F4F8;
+    return func_002EBC50(a0);
+}
+
+/* clone */
+
+
+
+__attribute__((section(".text.func_002F6CD0")))
+int func_002F6CD0(void *a0) {
+    *(char**)((char*)a0+0xF0) = D_0044F598;
+    return func_002FBE28(a0);
+}
+
+/* clone */
+
+
+
+__attribute__((section(".text.func_002F7178")))
+int func_002F7178(void *a0) {
+    *(char**)((char*)a0+0xF0) = D_0044F6D8;
+    return func_002EBC50(a0);
+}
+
+/* clone */
+
+
+__attribute__((section(".text.func_002FFB10")))
+void *func_002FFB10(void *a0) {
+    void *r = a0;
+    if (r)
+        r = func_002E3480(a0);
+    return r;
+}
