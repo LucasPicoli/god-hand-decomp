cd85e87b11d4ff1c64c1ccbb05191e7870f21b86
Lucas Picoli <LucasPicoli@users.noreply.github.com>
2026-08-02 11:39:43 -0700

    match: +10 functions (func_00297D40..func_00297FC8)

---
+++ src/cod/00297d40.c
@@ -0,0 +1,102 @@
+/* sn-2.95.3-136 matched TU. */
+
+extern void func_00297D30();
+extern void func_00297D78();
+extern void func_00297DC0();
+extern void func_00297E08();
+extern void func_00297E50();
+extern void func_00297E98();
+extern void func_00297EE0();
+extern void func_00297F28();
+extern void func_00297F70();
+extern void func_00297FB8();
+
+__attribute__((section(".text.func_00297D40")))
+void func_00297D40(void *a0, char *a1) {
+    if (a1) {
+        func_00297D30(a0, a1 + 0x10, *(int *)(a1 + 4));
+    } else {
+        func_00297D30(a0, 0, 0);
+    }
+}
+
+__attribute__((section(".text.func_00297D88")))
+void func_00297D88(void *a0, char *a1) {
+    if (a1) {
+        func_00297D78(a0, a1 + 0x10, *(int *)(a1 + 4));
+    } else {
+        func_00297D78(a0, 0, 0);
+    }
+}
+
+__attribute__((section(".text.func_00297DD0")))
+void func_00297DD0(void *a0, char *a1) {
+    if (a1) {
+        func_00297DC0(a0, a1 + 0x10, *(int *)(a1 + 4));
+    } else {
+        func_00297DC0(a0, 0, 0);
+    }
+}
+
+__attribute__((section(".text.func_00297E18")))
+void func_00297E18(void *a0, char *a1) {
+    if (a1) {
+        func_00297E08(a0, a1 + 0x10, *(int *)(a1 + 4));
+    } else {
+        func_00297E08(a0, 0, 0);
+    }
+}
+
+__attribute__((section(".text.func_00297E60")))
+void func_00297E60(void *a0, char *a1) {
+    if (a1) {
+        func_00297E50(a0, a1 + 0x10, *(int *)(a1 + 4));
+    } else {
+        func_00297E50(a0, 0, 0);
+    }
+}
+
+__attribute__((section(".text.func_00297EA8")))
+void func_00297EA8(void *a0, char *a1) {
+    if (a1) {
+        func_00297E98(a0, a1 + 0x10, *(int *)(a1 + 4));
+    } else {
+        func_00297E98(a0, 0, 0);
+    }
+}
+
+__attribute__((section(".text.func_00297EF0")))
+void func_00297EF0(void *a0, char *a1) {
+    if (a1) {
+        func_00297EE0(a0, a1 + 0x10, *(int *)(a1 + 4));
+    } else {
+        func_00297EE0(a0, 0, 0);
+    }
+}
+
+__attribute__((section(".text.func_00297F38")))
+void func_00297F38(void *a0, char *a1) {
+    if (a1) {
+        func_00297F28(a0, a1 + 0x10, *(int *)(a1 + 4));
+    } else {
+        func_00297F28(a0, 0, 0);
+    }
+}
+
+__attribute__((section(".text.func_00297F80")))
+void func_00297F80(void *a0, char *a1) {
+    if (a1) {
+        func_00297F70(a0, a1 + 0x10, *(int *)(a1 + 4));
+    } else {
+        func_00297F70(a0, 0, 0);
+    }
+}
+
+__attribute__((section(".text.func_00297FC8")))
+void func_00297FC8(void *a0, char *a1) {
+    if (a1) {
+        func_00297FB8(a0, a1 + 0x10, *(int *)(a1 + 4));
+    } else {
+        func_00297FB8(a0, 0, 0);
+    }
+}
