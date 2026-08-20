7d27758901b45cc9905aa330d62422bcd2ad9c3a
Lucas Picoli <LucasPicoli@users.noreply.github.com>
2026-08-20 01:11:46 -0700

    match: +1 functions (func_003A4CF0)

---
+++ src/cod/003a4cf0.c
@@ -0,0 +1,29 @@
+/* ee-2.9-991111 matched TU. */
+
+typedef union
+{
+  float value;
+  unsigned int word;
+} ieee_float_shape_type;
+
+#define GET_FLOAT_WORD(i,d)					\
+do {								\
+  ieee_float_shape_type gf_u;					\
+  gf_u.value = (d);						\
+  (i) = gf_u.word;						\
+} while (0)
+
+#define SET_FLOAT_WORD(d,i)					\
+do {								\
+  ieee_float_shape_type sf_u;					\
+  sf_u.word = (i);						\
+  (d) = sf_u.value;						\
+} while (0)
+
+__attribute__((section(".text.func_003A4CF0")))
+int func_003A4CF0(float x)
+{
+	int ix;
+	GET_FLOAT_WORD(ix,x);
+	return (int)((unsigned int)((ix&0x7fffffff)-0x7f800000)>>31);
+}
