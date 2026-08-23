420bd939fd2ce9a8634d6338a5d52837c737ea17
Lucas Picoli <LucasPicoli@users.noreply.github.com>
2026-08-22 19:04:18 -0700

    match: +1 functions (func_002D9C10)

---
+++ src/cod/002d9c10.c
@@ -0,0 +1,40 @@
+/* sn-2.95.3-136 matched TU. */
+
+extern char D_00747A78[];
+
+/* compiler: sn-2.95.3-136 ; extra keys: none */
+
+
+__attribute__((section(".text.func_002D9C10")))
+void func_002D9C10(int *a0) {
+    char *p;
+    int f;
+    long t;
+    p = D_00747A78;
+    if ((*(int *)D_00747A78 & 0x2000) != 0) {
+        return;
+    }
+    t = *(int *)(p - 0x54);
+    if ((((unsigned long)t >> 3) & 1UL) == 1UL) {
+        return;
+    }
+    f = *(int *)(p + 0xC);
+    if ((f & 0x2000000) != 0) {
+        return;
+    }
+    p = p - 0x5D8;
+    if (*(unsigned short *)(p + 0x5B0) == 0x21) {
+        return;
+    }
+    if ((*(int *)(p + 0x594) & 0x800) != 0) {
+        if ((f & 0x8000000) != 0) {
+            return;
+        }
+    }
+    if (*(unsigned char *)(p + 0x569) == 1) {
+        if ((*(int *)(p + 0x210) & 1) == 0) {
+            return;
+        }
+    }
+    *a0 = *a0 + 1;
+}
