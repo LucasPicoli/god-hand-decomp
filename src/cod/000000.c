1d2e40a792b7016ce902c38e2e3279ae7e7c7762
Lucas Picoli <LucasPicoli@users.noreply.github.com>
2026-08-31 00:52:24 -0700

    match: +1 functions (func_0028C400: external-rodata jumptable class, sn, via extern_jtbl)

--- src/cod/000000.c
+++ src/cod/000000.c
@@ -3817,3 +3817,4 @@ INCLUDE_ASM("nonmatching", SearchData);
 
 
 
+
---
+++ src/cod/0028c400.c
@@ -0,0 +1,437 @@
+/* sn-2.95.3-136 matched TU. */
+
+extern int cModel_setTextureExchange(void *self, void *tex, void *tbl, int n);
+extern int cModel_setupModel(void *a, void *b, void *c, int d, int e);
+
+/* sn-2.95.3-136 matched TU. */
+
+
+
+
+__attribute__((section(".text.func_0028C400")))
+int func_0028C400(void *a0)
+{
+    char *s0 = (char *)a0;
+    int s1;
+    int s2;
+
+    switch (*(int *)(s0 + 0x564)) {
+    case 0x204:
+    default:
+        {
+            int q0 = *(volatile int *)(s0 + 0x304);
+            s2 = *(int *)(q0 + 0x158) + q0;
+            s1 = *(int *)(q0 + 0x154) + q0;
+        }
+        break;
+    case 0x207:
+        {
+            int q1 = *(volatile int *)(s0 + 0x304);
+            s2 = *(int *)(q1 + 0x100) + q1;
+            s1 = *(int *)(q1 + 0xFC) + q1;
+        }
+        break;
+    case 0x208:
+        {
+            int q2 = *(volatile int *)(s0 + 0x304);
+            s2 = *(int *)(q2 + 0x1A0) + q2;
+            s1 = *(int *)(q2 + 0x19C) + q2;
+        }
+        break;
+    case 0x209:
+        {
+            int q3 = *(volatile int *)(s0 + 0x304);
+            s2 = *(int *)(q3 + 0x1D0) + q3;
+            s1 = *(int *)(q3 + 0x1CC) + q3;
+        }
+        break;
+    case 0x20B:
+        {
+            int q4 = *(volatile int *)(s0 + 0x304);
+            s2 = *(int *)(q4 + 0x1A8) + q4;
+            s1 = *(int *)(q4 + 0x1A4) + q4;
+        }
+        break;
+    case 0x20D:
+        {
+            int q5 = *(volatile int *)(s0 + 0x304);
+            s2 = *(int *)(q5 + 0x110) + q5;
+            s1 = *(int *)(q5 + 0x10C) + q5;
+        }
+        break;
+    case 0x20E:
+        {
+            int q6 = *(volatile int *)(s0 + 0x304);
+            s2 = *(int *)(q6 + 0x1F0) + q6;
+            s1 = *(int *)(q6 + 0x1EC) + q6;
+        }
+        break;
+    case 0x20F:
+        {
+            int q7 = *(volatile int *)(s0 + 0x304);
+            s2 = *(int *)(q7 + 0x148) + q7;
+            s1 = *(int *)(q7 + 0x144) + q7;
+        }
+        break;
+    case 0x213:
+        {
+            int q8 = *(volatile int *)(s0 + 0x304);
+            s2 = *(int *)(q8 + 0x1E0) + q8;
+            s1 = *(int *)(q8 + 0x1DC) + q8;
+        }
+        break;
+    case 0x214:
+        {
+            int q9 = *(volatile int *)(s0 + 0x304);
+            s2 = *(int *)(q9 + 0x1B0) + q9;
+            s1 = *(int *)(q9 + 0x1AC) + q9;
+        }
+        break;
+    case 0x215:
+        {
+            int q10 = *(volatile int *)(s0 + 0x304);
+            s2 = *(int *)(q10 + 0x1B8) + q10;
+            s1 = *(int *)(q10 + 0x1B4) + q10;
+        }
+        break;
+    case 0x216:
+        {
+            int q11 = *(volatile int *)(s0 + 0x304);
+            s2 = *(int *)(q11 + 0x128) + q11;
+            s1 = *(int *)(q11 + 0x124) + q11;
+        }
+        break;
+    case 0x217:
+        {
+            int q12 = *(volatile int *)(s0 + 0x304);
+            s2 = *(int *)(q12 + 0x1E8) + q12;
+            s1 = *(int *)(q12 + 0x1E4) + q12;
+        }
+        break;
+    case 0x21A:
+        {
+            int q13 = *(volatile int *)(s0 + 0x304);
+            s2 = *(int *)(q13 + 0x168) + q13;
+            s1 = *(int *)(q13 + 0x164) + q13;
+        }
+        break;
+    case 0x21B:
+        {
+            int q14 = *(volatile int *)(s0 + 0x304);
+            s2 = *(int *)(q14 + 0x140) + q14;
+            s1 = *(int *)(q14 + 0x13C) + q14;
+        }
+        break;
+    case 0x21C:
+        {
+            int q15 = *(volatile int *)(s0 + 0x304);
+            s2 = *(int *)(q15 + 0x198) + q15;
+            s1 = *(int *)(q15 + 0x194) + q15;
+        }
+        break;
+    case 0x21D:
+        {
+            int q16 = *(volatile int *)(s0 + 0x304);
+            s2 = *(int *)(q16 + 0x118) + q16;
+            s1 = *(int *)(q16 + 0x114) + q16;
+        }
+        break;
+    case 0x21E:
+        {
+            int q17 = *(volatile int *)(s0 + 0x304);
+            s2 = *(int *)(q17 + 0x160) + q17;
+            s1 = *(int *)(q17 + 0x15C) + q17;
+        }
+        break;
+    case 0x21F:
+        {
+            int q18 = *(volatile int *)(s0 + 0x304);
+            s2 = *(int *)(q18 + 0x1D8) + q18;
+            s1 = *(int *)(q18 + 0x1D4) + q18;
+        }
+        break;
+    case 0x240:
+        {
+            int q19 = *(volatile int *)(s0 + 0x304);
+            s2 = *(int *)(q19 + 0xF0) + q19;
+            s1 = *(int *)(q19 + 0xEC) + q19;
+        }
+        break;
+    case 0x241:
+        {
+            int q20 = *(volatile int *)(s0 + 0x304);
+            s2 = *(int *)(q20 + 0x188) + q20;
+            s1 = *(int *)(q20 + 0x184) + q20;
+        }
+        break;
+    case 0x242:
+        {
+            int q21 = *(volatile int *)(s0 + 0x304);
+            s2 = *(int *)(q21 + 0x130) + q21;
+            s1 = *(int *)(q21 + 0x12C) + q21;
+        }
+        break;
+    case 0x245:
+        {
+            int q22 = *(volatile int *)(s0 + 0x304);
+            s2 = *(int *)(q22 + 0x170) + q22;
+            s1 = *(int *)(q22 + 0x16C) + q22;
+        }
+        break;
+    case 0x246:
+        {
+            int q23 = *(volatile int *)(s0 + 0x304);
+            s2 = *(int *)(q23 + 0x138) + q23;
+            s1 = *(int *)(q23 + 0x134) + q23;
+        }
+        break;
+    case 0x249:
+        {
+            int q24 = *(volatile int *)(s0 + 0x304);
+            s2 = *(int *)(q24 + 0xF8) + q24;
+            s1 = *(int *)(q24 + 0xF4) + q24;
+        }
+        break;
+    case 0x250:
+        {
+            int q25 = *(volatile int *)(s0 + 0x304);
+            s2 = *(int *)(q25 + 0x1F8) + q25;
+            s1 = *(int *)(q25 + 0x1F4) + q25;
+        }
+        break;
+    case 0x252:
+        {
+            int q26 = *(volatile int *)(s0 + 0x304);
+            s2 = *(int *)(q26 + 0x200) + q26;
+            s1 = *(int *)(q26 + 0x1FC) + q26;
+        }
+        break;
+    case 0x254:
+        {
+            int q27 = *(volatile int *)(s0 + 0x304);
+            s2 = *(int *)(q27 + 0x208) + q27;
+            s1 = *(int *)(q27 + 0x204) + q27;
+        }
+        break;
+    case 0x256:
+        {
+            int q28 = *(volatile int *)(s0 + 0x304);
+            s2 = *(int *)(q28 + 0x210) + q28;
+            s1 = *(int *)(q28 + 0x20C) + q28;
+        }
+        break;
+    case 0x270:
+        {
+            int q29 = *(volatile int *)(s0 + 0x304);
+            s2 = *(int *)(q29 + 0x1C0) + q29;
+            s1 = *(int *)(q29 + 0x1BC) + q29;
+        }
+        break;
+    case 0x274:
+        {
+            int q30 = *(volatile int *)(s0 + 0x304);
+            s2 = *(int *)(q30 + 0x1C8) + q30;
+            s1 = *(int *)(q30 + 0x1C4) + q30;
+        }
+        break;
+    case 0x275:
+        {
+            int q31 = *(volatile int *)(s0 + 0x304);
+            s2 = *(int *)(q31 + 0x108) + q31;
+            s1 = *(int *)(q31 + 0x104) + q31;
+        }
+        break;
+    case 0x278:
+        {
+            int q32 = *(volatile int *)(s0 + 0x304);
+            s2 = *(int *)(q32 + 0x178) + q32;
+            s1 = *(int *)(q32 + 0x174) + q32;
+        }
+        break;
+    case 0x279:
+        {
+            int q33 = *(volatile int *)(s0 + 0x304);
+            s2 = *(int *)(q33 + 0x180) + q33;
+            s1 = *(int *)(q33 + 0x17C) + q33;
+        }
+        break;
+    case 0x2A2:
+        {
+            int q34 = *(volatile int *)(s0 + 0x304);
+            s2 = *(int *)(q34 + 0x120) + q34;
+            s1 = *(int *)(q34 + 0x11C) + q34;
+        }
+        break;
+    case 0x2A5:
+        {
+            int q35 = *(volatile int *)(s0 + 0x304);
+            s2 = *(int *)(q35 + 0x190) + q35;
+            s1 = *(int *)(q35 + 0x18C) + q35;
+        }
+        break;
+    case 0x2A7:
+        {
+            int q36 = *(volatile int *)(s0 + 0x304);
+            s2 = *(int *)(q36 + 0x150) + q36;
+            s1 = *(int *)(q36 + 0x14C) + q36;
+        }
+        break;
+    case 0x2FF:
+        {
+            int q37 = *(volatile int *)(s0 + 0x304);
+            s2 = *(int *)(q37 + 0x218) + q37;
+            s1 = *(int *)(q37 + 0x214) + q37;
+        }
+        break;
+    }
+    switch (*(int *)(s0 + 0x564)) {
+    case 0x200:
+        if (*(int *)(s0 + 0x153C) & 0x1) {
+            char *tC6D4 = *(char **)(s0 + 0x304);
+            *(int *)(s0 + 0x254) = *(int *)(s0 + 0x254) | 0x10000000;
+            cModel_setTextureExchange(s0, (void *)s1, (void *)(*(int *)(tC6D4 + 0x4) + (int)tC6D4), 0);
+            *(int *)(s0 + 0x564) = 0x227;
+        }
+        break;
+    case 0x201:
+        if (*(int *)(s0 + 0x153C) & 0x1) {
+            char *tC714 = *(char **)(s0 + 0x304);
+            *(int *)(s0 + 0x254) = *(int *)(s0 + 0x254) | 0x10000000;
+            cModel_setTextureExchange(s0, (void *)s1, (void *)(*(int *)(tC714 + 0x8) + (int)tC714), 0);
+            *(int *)(s0 + 0x564) = 0x228;
+        }
+        break;
+    case 0x202:
+        if (*(int *)(s0 + 0x153C) & 0x1) {
+            char *tC754 = *(char **)(s0 + 0x304);
+            *(int *)(s0 + 0x254) = *(int *)(s0 + 0x254) | 0x10000000;
+            cModel_setTextureExchange(s0, (void *)s1, (void *)(*(int *)(tC754 + 0xC) + (int)tC754), 0);
+            *(int *)(s0 + 0x564) = 0x229;
+        }
+        break;
+    case 0x203:
+        if (*(int *)(s0 + 0x153C) & 0x1) {
+            char *tC794 = *(char **)(s0 + 0x304);
+            *(int *)(s0 + 0x254) = *(int *)(s0 + 0x254) | 0x10000000;
+            cModel_setTextureExchange(s0, (void *)s1, (void *)(*(int *)(tC794 + 0x10) + (int)tC794), 0);
+            *(int *)(s0 + 0x564) = 0x22A;
+        }
+        break;
+    case 0x204:
+        if (*(int *)(s0 + 0x153C) & 0x1) {
+            char *tC7D4 = *(char **)(s0 + 0x304);
+            *(int *)(s0 + 0x254) = *(int *)(s0 + 0x254) | 0x10000000;
+            cModel_setTextureExchange(s0, (void *)s1, (void *)(*(int *)(tC7D4 + 0x14) + (int)tC7D4), 0);
+            *(int *)(s0 + 0x564) = 0x22B;
+        }
+        break;
+    case 0x240:
+        if (*(int *)(s0 + 0x153C) & 0x1) {
+            char *tC814 = *(char **)(s0 + 0x304);
+            *(int *)(s0 + 0x254) = *(int *)(s0 + 0x254) | 0x10000000;
+            cModel_setTextureExchange(s0, (void *)s1, (void *)(*(int *)(tC814 + 0x24) + (int)tC814), 0);
+            *(int *)(s0 + 0x564) = 0x24A;
+        }
+        break;
+    case 0x21A:
+        if (*(int *)(s0 + 0x153C) & 0x1) {
+            char *tC854 = *(char **)(s0 + 0x304);
+            *(int *)(s0 + 0x254) = *(int *)(s0 + 0x254) | 0x10000000;
+            cModel_setTextureExchange(s0, (void *)s1, (void *)(*(int *)(tC854 + 0x18) + (int)tC854), 0);
+            *(int *)(s0 + 0x564) = 0x22C;
+        }
+        break;
+    case 0x21D:
+        if (*(int *)(s0 + 0x153C) & 0x1) {
+            char *tC894 = *(char **)(s0 + 0x304);
+            *(int *)(s0 + 0x254) = *(int *)(s0 + 0x254) | 0x10000000;
+            cModel_setTextureExchange(s0, (void *)s1, (void *)(*(int *)(tC894 + 0x30) + (int)tC894), 0);
+            *(int *)(s0 + 0x564) = 0x22E;
+        }
+        break;
+    case 0x21E:
+        if (*(int *)(s0 + 0x153C) & 0x1) {
+            char *tC8D4 = *(char **)(s0 + 0x304);
+            *(int *)(s0 + 0x254) = *(int *)(s0 + 0x254) | 0x10000000;
+            cModel_setTextureExchange(s0, (void *)s1, (void *)(*(int *)(tC8D4 + 0x28) + (int)tC8D4), 0);
+            *(int *)(s0 + 0x564) = 0x22F;
+        }
+        break;
+    case 0x248:
+        if (*(int *)(s0 + 0x153C) & 0x1) {
+            char *tC914 = *(char **)(s0 + 0x304);
+            *(int *)(s0 + 0x254) = *(int *)(s0 + 0x254) | 0x10000000;
+            cModel_setTextureExchange(s0, (void *)s1, (void *)(*(int *)(tC914 + 0x34) + (int)tC914), 0);
+            *(int *)(s0 + 0x564) = 0x24C;
+        }
+        break;
+    case 0x249:
+        if (*(int *)(s0 + 0x153C) & 0x1) {
+            char *tC954 = *(char **)(s0 + 0x304);
+            *(int *)(s0 + 0x254) = *(int *)(s0 + 0x254) | 0x10000000;
+            cModel_setTextureExchange(s0, (void *)s1, (void *)(*(int *)(tC954 + 0x48) + (int)tC954), 0);
+            *(int *)(s0 + 0x564) = 0x24E;
+        }
+        break;
+    case 0x216:
+        if (*(int *)(s0 + 0x153C) & 0x1) {
+            char *tC994 = *(char **)(s0 + 0x304);
+            *(int *)(s0 + 0x254) = *(int *)(s0 + 0x254) | 0x10000000;
+            cModel_setTextureExchange(s0, (void *)s1, (void *)(*(int *)(tC994 + 0x2C) + (int)tC994), 0);
+            *(int *)(s0 + 0x564) = 0x24B;
+        }
+        break;
+    case 0x21B:
+        if (*(int *)(s0 + 0x153C) & 0x1) {
+            char *tC9D4 = *(char **)(s0 + 0x304);
+            *(int *)(s0 + 0x254) = *(int *)(s0 + 0x254) | 0x10000000;
+            cModel_setTextureExchange(s0, (void *)s1, (void *)(*(int *)(tC9D4 + 0x38) + (int)tC9D4), 0);
+            *(int *)(s0 + 0x564) = 0x22D;
+        }
+        break;
+    case 0x225:
+        if (*(int *)(s0 + 0x153C) & 0x1) {
+            char *tCA14 = *(char **)(s0 + 0x304);
+            *(int *)(s0 + 0x254) = *(int *)(s0 + 0x254) | 0x10000000;
+            cModel_setTextureExchange(s0, (void *)s1, (void *)(*(int *)(tCA14 + 0x3C) + (int)tCA14), 0);
+            *(int *)(s0 + 0x564) = 0x24D;
+        }
+        break;
+    case 0x242:
+        if (*(int *)(s0 + 0x153C) & 0x1) {
+            char *tCA54 = *(char **)(s0 + 0x304);
+            *(int *)(s0 + 0x254) = *(int *)(s0 + 0x254) | 0x10000000;
+            cModel_setTextureExchange(s0, (void *)s1, (void *)(*(int *)(tCA54 + 0x40) + (int)tCA54), 0);
+            *(int *)(s0 + 0x564) = 0x243;
+        }
+        break;
+    case 0x270:
+    case 0x271:
+    case 0x272:
+    case 0x273:
+    case 0x274:
+        if (*(int *)(s0 + 0x153C) & 0x1) {
+            char *tCA94 = *(char **)(s0 + 0x304);
+            *(int *)(s0 + 0x254) = *(int *)(s0 + 0x254) | 0x10000000;
+            cModel_setTextureExchange(s0, (void *)s1, (void *)(*(int *)(tCA94 + 0x1C) + (int)tCA94), 0);
+            *(int *)(s0 + 0x564) = 0x271;
+        }
+        if (*(int *)(s0 + 0x153C) & 0x2) {
+            char *tCAD4 = *(char **)(s0 + 0x304);
+            *(int *)(s0 + 0x254) = *(int *)(s0 + 0x254) | 0x10000000;
+            cModel_setTextureExchange(s0, (void *)s1, (void *)(*(int *)(tCAD4 + 0x1C) + (int)tCAD4), 1);
+            *(int *)(s0 + 0x564) = 0x272;
+        }
+        if (*(int *)(s0 + 0x153C) & 0x4) {
+            char *tCB14 = *(char **)(s0 + 0x304);
+            *(int *)(s0 + 0x254) = *(int *)(s0 + 0x254) | 0x10000000;
+            cModel_setTextureExchange(s0, (void *)s1, (void *)(*(int *)(tCB14 + 0x1C) + (int)tCB14), 2);
+            *(int *)(s0 + 0x564) = 0x273;
+        }
+        break;
+    }
+    if (s1 != 0) {
+        return cModel_setupModel(s0, (void *)s2, (void *)s1, 0, 0);
+    }
+    return 0;
+}
