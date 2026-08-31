38b6231c13f2824ac80d00d9bea72985d064ead8
Lucas Picoli <LucasPicoli@users.noreply.github.com>
2026-08-31 00:16:25 -0700

    match: +1 functions (cEm00_GetPlMotion: external-rodata jumptable class, sn, via extern_jtbl)

---
+++ src/cod/0023a300.c
@@ -0,0 +1,654 @@
+/* sn-2.95.3-136 matched TU. */
+
+extern void *Obj0000_Get_D_00747A94_2DB6B0();
+extern unsigned int func_0031ED08(float f12);
+extern void func_002A8578(void *a0, int a1, int a2, float f, int a3, int t0, int t1);
+
+/* SN ProDG ee-gcc 2.95.3 matched TU. */
+
+
+
+
+
+__attribute__((section(".text.cEm00_GetPlMotion")))
+void cEm00_GetPlMotion(void *a0, int a1, float f12, float f13)
+{
+    char *s0 = (char *)a0;
+    int s1;
+    int t0;
+    void *s2;
+
+    s2 = Obj0000_Get_D_00747A94_2DB6B0();
+    switch (a1) {
+    default:
+    case 0:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0xC80) + v0;
+        t0 = *(int *)(v0 + 0xC84) + v0;
+        }
+        break;
+    case 1:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0xC88) + v0;
+        t0 = *(int *)(v0 + 0xC8C) + v0;
+        }
+        break;
+    case 2:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0xC90) + v0;
+        t0 = *(int *)(v0 + 0xC94) + v0;
+        }
+        break;
+    case 3:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x1A7C) + v0;
+        t0 = *(int *)(v0 + 0x1A80) + v0;
+        }
+        break;
+    case 4:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x1A84) + v0;
+        t0 = *(int *)(v0 + 0x1A88) + v0;
+        }
+        break;
+    case 5:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x1A8C) + v0;
+        t0 = *(int *)(v0 + 0x1A90) + v0;
+        }
+        break;
+    case 6:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x520) + v0;
+        t0 = *(int *)(v0 + 0x524) + v0;
+        }
+        break;
+    case 7:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x808) + v0;
+        t0 = *(int *)(v0 + 0x80C) + v0;
+        }
+        break;
+    case 8:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x7F8) + v0;
+        t0 = *(int *)(v0 + 0x7FC) + v0;
+        }
+        break;
+    case 9:
+        {
+        if (*(short *)((char *)s2 + 0x54A) <= 0) {
+            int v0 = *(int *)(s0 + 0x304);
+            s1 = *(int *)(v0 + 0x810) + v0;
+            t0 = *(int *)(v0 + 0x814) + v0;
+        } else {
+            int v0 = *(int *)(s0 + 0x304);
+            s1 = *(int *)(v0 + 0x800) + v0;
+            t0 = *(int *)(v0 + 0x804) + v0;
+        }
+        }
+        break;
+    case 10:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x800) + v0;
+        t0 = *(int *)(v0 + 0x804) + v0;
+        }
+        break;
+    case 11:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x102C) + v0;
+        t0 = *(int *)(v0 + 0x1030) + v0;
+        }
+        break;
+    case 12:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x1014) + v0;
+        t0 = *(int *)(v0 + 0x1018) + v0;
+        }
+        break;
+    case 13:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x1024) + v0;
+        t0 = *(int *)(v0 + 0x1028) + v0;
+        }
+        break;
+    case 14:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x101C) + v0;
+        t0 = *(int *)(v0 + 0x1020) + v0;
+        }
+        break;
+    case 15:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x1DA0) + v0;
+        t0 = *(int *)(v0 + 0x1DA4) + v0;
+        }
+        break;
+    case 16:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x2F00) + v0;
+        t0 = *(int *)(v0 + 0x2F04) + v0;
+        }
+        break;
+    case 17:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x1BB0) + v0;
+        t0 = *(int *)(v0 + 0x1BB4) + v0;
+        }
+        break;
+    case 18:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x1BB8) + v0;
+        t0 = *(int *)(v0 + 0x1BBC) + v0;
+        }
+        break;
+    case 19:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x2428) + v0;
+        t0 = *(int *)(v0 + 0x242C) + v0;
+        }
+        break;
+    case 20:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x2430) + v0;
+        t0 = *(int *)(v0 + 0x2434) + v0;
+        }
+        break;
+    case 21:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x20E0) + v0;
+        t0 = *(int *)(v0 + 0x20E4) + v0;
+        }
+        break;
+    case 22:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x20E8) + v0;
+        t0 = *(int *)(v0 + 0x20EC) + v0;
+        }
+        break;
+    case 23:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x2128) + v0;
+        t0 = *(int *)(v0 + 0x212C) + v0;
+        }
+        break;
+    case 24:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x2130) + v0;
+        t0 = *(int *)(v0 + 0x2134) + v0;
+        }
+        break;
+    case 25:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x2138) + v0;
+        t0 = *(int *)(v0 + 0x213C) + v0;
+        }
+        break;
+    case 26:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x2140) + v0;
+        t0 = *(int *)(v0 + 0x2144) + v0;
+        }
+        break;
+    case 27:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x1558) + v0;
+        t0 = *(int *)(v0 + 0x155C) + v0;
+        }
+        break;
+    case 28:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x1474) + v0;
+        t0 = *(int *)(v0 + 0x1478) + v0;
+        }
+        break;
+    case 30:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x14B0) + v0;
+        t0 = *(int *)(v0 + 0x14B4) + v0;
+        }
+        break;
+    case 31:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x14B8) + v0;
+        t0 = *(int *)(v0 + 0x14BC) + v0;
+        }
+        break;
+    case 29:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x147C) + v0;
+        t0 = *(int *)(v0 + 0x1480) + v0;
+        }
+        break;
+    case 32:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x24D4) + v0;
+        t0 = *(int *)(v0 + 0x24D8) + v0;
+        }
+        break;
+    case 33:
+        {
+        if (*(short *)((char *)s2 + 0x54A) <= 0) {
+            int v0 = *(int *)(s0 + 0x304);
+            s1 = *(int *)(v0 + 0x106C) + v0;
+            t0 = *(int *)(v0 + 0x1070) + v0;
+        } else {
+            int v0 = *(int *)(s0 + 0x304);
+            s1 = *(int *)(v0 + 0x1064) + v0;
+            t0 = *(int *)(v0 + 0x1068) + v0;
+        }
+        }
+        break;
+    case 34:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x1E5C) + v0;
+        t0 = *(int *)(v0 + 0x1E60) + v0;
+        }
+        break;
+    case 35:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x36D8) + v0;
+        t0 = *(int *)(v0 + 0x36DC) + v0;
+        }
+        break;
+    case 36:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x36E0) + v0;
+        t0 = *(int *)(v0 + 0x36E4) + v0;
+        }
+        break;
+    case 37:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x36E8) + v0;
+        t0 = *(int *)(v0 + 0x36EC) + v0;
+        }
+        break;
+    case 56:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x538) + v0;
+        t0 = *(int *)(v0 + 0x53C) + v0;
+        }
+        break;
+    case 57:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x109C) + v0;
+        t0 = *(int *)(v0 + 0x10A0) + v0;
+        }
+        break;
+    case 58:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0xCB0) + v0;
+        t0 = *(int *)(v0 + 0xCB4) + v0;
+        }
+        break;
+    case 59:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0xCB8) + v0;
+        t0 = *(int *)(v0 + 0xCBC) + v0;
+        }
+        break;
+    case 60:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0xCC0) + v0;
+        t0 = *(int *)(v0 + 0xCC4) + v0;
+        }
+        break;
+    case 61:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x3C04) + v0;
+        t0 = *(int *)(v0 + 0x3C08) + v0;
+        }
+        break;
+    case 62:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x1840) + v0;
+        t0 = *(int *)(v0 + 0x1844) + v0;
+        }
+        break;
+    case 63:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x1848) + v0;
+        t0 = *(int *)(v0 + 0x184C) + v0;
+        }
+        break;
+    case 64:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x1850) + v0;
+        t0 = *(int *)(v0 + 0x1854) + v0;
+        }
+        break;
+    case 65:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x1858) + v0;
+        t0 = *(int *)(v0 + 0x185C) + v0;
+        }
+        break;
+    case 66:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x14E8) + v0;
+        t0 = *(int *)(v0 + 0x14EC) + v0;
+        }
+        break;
+    case 67:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x14F0) + v0;
+        t0 = *(int *)(v0 + 0x14F4) + v0;
+        }
+        break;
+    case 68:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x14F8) + v0;
+        t0 = *(int *)(v0 + 0x14FC) + v0;
+        }
+        break;
+    case 69:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x3204) + v0;
+        t0 = *(int *)(v0 + 0x3208) + v0;
+        }
+        break;
+    case 70:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x320C) + v0;
+        t0 = *(int *)(v0 + 0x3210) + v0;
+        }
+        break;
+    case 71:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x3214) + v0;
+        t0 = *(int *)(v0 + 0x3218) + v0;
+        }
+        break;
+    case 72:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x321C) + v0;
+        t0 = *(int *)(v0 + 0x3220) + v0;
+        }
+        break;
+    case 73:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x33F4) + v0;
+        t0 = *(int *)(v0 + 0x33F8) + v0;
+        }
+        break;
+    case 74:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x33FC) + v0;
+        t0 = *(int *)(v0 + 0x3400) + v0;
+        }
+        break;
+    case 75:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x3404) + v0;
+        t0 = *(int *)(v0 + 0x3408) + v0;
+        }
+        break;
+    case 76:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x39B8) + v0;
+        t0 = *(int *)(v0 + 0x39BC) + v0;
+        }
+        break;
+    case 77:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x39C0) + v0;
+        t0 = *(int *)(v0 + 0x39C4) + v0;
+        }
+        break;
+    case 78:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x39C8) + v0;
+        t0 = *(int *)(v0 + 0x39CC) + v0;
+        }
+        break;
+    case 79:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x3E64) + v0;
+        t0 = *(int *)(v0 + 0x3E68) + v0;
+        }
+        break;
+    case 80:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x3E6C) + v0;
+        t0 = *(int *)(v0 + 0x3E70) + v0;
+        }
+        break;
+    case 81:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x3E74) + v0;
+        t0 = *(int *)(v0 + 0x3E78) + v0;
+        }
+        break;
+    case 82:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x3E44) + v0;
+        t0 = *(int *)(v0 + 0x3E48) + v0;
+        }
+        break;
+    case 83:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x1DA8) + v0;
+        t0 = *(int *)(v0 + 0x1DAC) + v0;
+        }
+        break;
+    case 38:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x2C50) + v0;
+        t0 = *(int *)(v0 + 0x2C54) + v0;
+        }
+        break;
+    case 39:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x2C58) + v0;
+        t0 = *(int *)(v0 + 0x2C5C) + v0;
+        }
+        break;
+    case 40:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x3244) + v0;
+        t0 = *(int *)(v0 + 0x3248) + v0;
+        }
+        break;
+    case 41:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x324C) + v0;
+        t0 = *(int *)(v0 + 0x3250) + v0;
+        }
+        break;
+    case 42:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x325C) + v0;
+        t0 = *(int *)(v0 + 0x3260) + v0;
+        }
+        break;
+    case 43:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x3254) + v0;
+        t0 = *(int *)(v0 + 0x3258) + v0;
+        }
+        break;
+    case 44:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x33DC) + v0;
+        t0 = *(int *)(v0 + 0x33E0) + v0;
+        }
+        break;
+    case 45:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x33E4) + v0;
+        t0 = *(int *)(v0 + 0x33E8) + v0;
+        }
+        break;
+    case 46:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x39EC) + v0;
+        t0 = *(int *)(v0 + 0x39F0) + v0;
+        }
+        break;
+    case 47:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x39F4) + v0;
+        t0 = *(int *)(v0 + 0x39F8) + v0;
+        }
+        break;
+    case 48:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x3BE8) + v0;
+        t0 = *(int *)(v0 + 0x3BEC) + v0;
+        }
+        break;
+    case 49:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x3BF0) + v0;
+        t0 = *(int *)(v0 + 0x3BF4) + v0;
+        }
+        break;
+    case 50:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x3B40) + v0;
+        t0 = *(int *)(v0 + 0x3B44) + v0;
+        }
+        break;
+    case 51:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x148C) + v0;
+        t0 = *(int *)(v0 + 0x1490) + v0;
+        }
+        break;
+    case 52:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x3C64) + v0;
+        t0 = *(int *)(v0 + 0x3C68) + v0;
+        }
+        break;
+    case 53:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x3C6C) + v0;
+        t0 = *(int *)(v0 + 0x3C70) + v0;
+        }
+        break;
+    case 54:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x3C74) + v0;
+        t0 = *(int *)(v0 + 0x3C78) + v0;
+        }
+        break;
+    case 55:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x3C7C) + v0;
+        t0 = *(int *)(v0 + 0x3C80) + v0;
+        }
+        break;
+    case 84:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x2C6C) + v0;
+        t0 = *(int *)(v0 + 0x2C70) + v0;
+        }
+        break;
+    case 85:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x2C74) + v0;
+        t0 = *(int *)(v0 + 0x2C78) + v0;
+        }
+        break;
+    case 86:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x2C7C) + v0;
+        t0 = *(int *)(v0 + 0x2C80) + v0;
+        }
+        break;
+    case 87:
+        {
+        int v0 = *(int *)(s0 + 0x304);
+        s1 = *(int *)(v0 + 0x2C84) + v0;
+        t0 = *(int *)(v0 + 0x2C88) + v0;
+        }
+        break;
+    }
+    func_002A8578(s2, s1, t0, f13, func_0031ED08(f12), 0, 0);
+}
