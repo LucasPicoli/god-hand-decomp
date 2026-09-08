79bb5175b1046bb02bd591256783874ef09aa905
Lucas Picoli <LucasPicoli@users.noreply.github.com>
2026-09-08 00:45:36 -0700

    match: +1 functions (func_0038B530)

---
+++ src/cod/0038b530.c
@@ -0,0 +1,10 @@
+/* ee-2.9-991111 matched TU. */
+
+extern int *Obj0000_Get_D_003FA62C_3A4FB8(void);
+
+__attribute__((section(".text.func_0038B530")))
+int func_0038B530(char *name)
+{
+    *Obj0000_Get_D_003FA62C_3A4FB8() = 5;
+    return -1;
+}
