2a95f0b82872fd29dcfbeda67c41c1135ab0fe49
Lucas Picoli <LucasPicoli@users.noreply.github.com>
2026-09-10 01:12:13 -0700

    match: +1 functions (mwlSfdSleepDecSvr)

---
+++ src/cod/003497a0.c
@@ -0,0 +1,18 @@
+/* cygnus-2.96 matched TU. */
+
+extern void Forward3462D8_3466C0(void);
+extern void func_00349738(void *h, int n);
+extern void Forward33BBD8_348C78(void);
+extern void Forward346368_3466D8(void);
+
+
+__attribute__((section(".text.mwlSfdSleepDecSvr")))
+int mwlSfdSleepDecSvr(void *h)
+{
+    Forward3462D8_3466C0();
+    func_00349738(h, 1);
+    Forward33BBD8_348C78();
+    func_00349738(h, 0);
+    Forward346368_3466D8();
+    return func_00348CB8(h);
+}
