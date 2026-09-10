d921913b5b265dd4f4e4655484e72aa6e49e913a
Lucas Picoli <LucasPicoli@users.noreply.github.com>
2026-09-10 01:00:29 -0700

    match: +1 functions (SFUO_ExecServer)

---
+++ src/cod/0035cf80.c
@@ -0,0 +1,16 @@
+/* cygnus-2.96 matched TU. */
+
+extern void func_0035CFC8(void *h);
+extern int Obj35D0_ReturnZero_D0D8(void *h);
+extern void func_0035D050(void *h);
+
+__attribute__((section(".text.SFUO_ExecServer")))
+int SFUO_ExecServer(void *h)
+{
+    int r;
+
+    func_0035CFC8(h);
+    r = Obj35D0_ReturnZero_D0D8(h);
+    func_0035D050(h);
+    return r;
+}
