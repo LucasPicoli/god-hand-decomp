339d63956d115bf898496737efcaeb8d1aaa48d8
Lucas Picoli <LucasPicoli@users.noreply.github.com>
2026-09-10 01:35:10 -0700

    match: +1 functions (sceSifRemoveRpcQueue)

---
+++ src/cod/003b3568.c
@@ -0,0 +1,46 @@
+/* ee-2.9-991111 matched TU. */
+
+/* ee-2.9-991111 */
+
+typedef struct rpcq {
+    void *f0;
+    int f4;
+    int f8;
+    int fC;
+    int f10;
+    struct rpcq *next;
+} RpcQ;
+
+typedef struct {
+    char pad0[0x28];
+    RpcQ *q;
+} RpcData;
+
+
+
+extern RpcData D_0077FF40;
+
+__attribute__((section(".text.sceSifRemoveRpcQueue")))
+RpcQ *sceSifRemoveRpcQueue(RpcQ *q)
+{
+    RpcQ *p;
+
+    func_003B63A0();
+    {
+        RpcData *rd = &D_0077FF40;
+        p = rd->q;
+        if (p == q) {
+            rd->q = p->next;
+        } else {
+            while (p != 0) {
+                if (p->next == q) {
+                    p->next = q->next;
+                    break;
+                }
+                p = p->next;
+            }
+        }
+    }
+    func_003B63F0();
+    return p;
+}
