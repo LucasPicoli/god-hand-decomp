b3bfba72bf8ac16fafe3fd682ae788a5bb5a8508
Lucas Picoli <LucasPicoli@users.noreply.github.com>
2026-09-10 01:16:25 -0700

    match: +1 functions (mwsfcre_UsrFree)

---
+++ src/cod/003446d8.c
@@ -0,0 +1,19 @@
+/* cygnus-2.96 matched TU. */
+
+typedef struct W {
+    char pad0[0x28];
+    void *(*alloc)(void *obj, int n);
+    void (*freep)(void *obj, void *p);
+    void *obj;
+} W;
+
+extern W *GetD003EC7E0_3452B0(void);
+
+__attribute__((section(".text.mwsfcre_UsrFree")))
+void mwsfcre_UsrFree(void *p)
+{
+    W *w;
+
+    w = GetD003EC7E0_3452B0();
+    w->freep(w->obj, p);
+}
