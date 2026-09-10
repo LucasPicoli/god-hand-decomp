eb94db1486e8a322a63943457810318726b4eacf
Lucas Picoli <LucasPicoli@users.noreply.github.com>
2026-09-10 01:09:37 -0700

    match: +1 functions (mwPlyFxSetLumiPrm)

--- src/cod/000000.c
+++ src/cod/000000.c
@@ -3573,3 +3573,4 @@ INCLUDE_ASM("nonmatching", SearchData);
 
 
 
+
---
+++ src/cod/00347700.c
@@ -0,0 +1,12 @@
+/* cygnus-2.96 matched TU. */
+
+extern int Forward36CC80_35FD58(void *p, int a, int b, int c);
+
+__attribute__((section(".text.mwPlyFxSetLumiPrm")))
+int mwPlyFxSetLumiPrm(void *h, int a, int b, int c)
+{
+    void *p;
+
+    p = func_00346C48(h);
+    return Forward36CC80_35FD58(p, a, b, c == 1);
+}
