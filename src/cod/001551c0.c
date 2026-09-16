78f492062ce5dc43d448051aaed4ce1c197cca3b
Lucas Picoli <LucasPicoli@users.noreply.github.com>
2026-09-15 23:59:53 -0700

    match: +6 functions (func_001551C0..func_001BBB28)

---
+++ src/cod/001551c0.c
@@ -0,0 +1,43 @@
+/* cygnus-2.96 matched TU. */
+
+extern void func_001B6FB8(void *a0);
+
+__attribute__((section(".text.func_001551C0")))
+int func_001551C0(void *a0, int a1)
+{
+    return func_001551E0(a0, a1) + 0x40;
+}
+
+__attribute__((section(".text.func_001552D0")))
+int func_001552D0(void *a0, int a1)
+{
+    return func_001552F0(a0, a1) + 0x40;
+}
+
+__attribute__((section(".text.func_0017F5D0")))
+int func_0017F5D0(void *a0)
+{
+    func_001B6FB8(a0);
+    return 1;
+}
+
+__attribute__((section(".text.func_00195EF8")))
+int func_00195EF8(void *a0)
+{
+    func_001B6FB8(a0);
+    return 1;
+}
+
+__attribute__((section(".text.func_001A8758")))
+int func_001A8758(void *a0)
+{
+    func_001B6FB8(a0);
+    return 1;
+}
+
+__attribute__((section(".text.func_001BBB28")))
+int func_001BBB28(void *a0)
+{
+    func_001B6FB8(a0);
+    return 1;
+}
